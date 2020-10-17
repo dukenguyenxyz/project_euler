9f9d074a1d7132a37ba6446bfccad66f1010d24e
dukeraphaelng <duke.raphael.ng@gmail.com>
2020-10-17 18:52:31 +1100

    feat: add problem 1 and update problem 7

--- problem_7_prime_number.cr
+++ problem_7_prime_number.cr
@@ -3,19 +3,35 @@ require "spec"
 module PrimeNumber
   extend self
 
-  def list(size : Int32)
-    self.calculate(size)
-  end
-
-  def at(position : Int32)
-    self.calculate(position)[-1]
-  end
-
-  private def self.calculate(position : Int32)
+  def below(limit : Int32)
     cur_num = 2
     primes = [2]
 
-    until primes.size == position
+    while cur_num < limit
+      cur_num += 1
+      primes.push(cur_num) if primes.all? { |p| !cur_num.divisible_by?(p) }
+    end
+
+    primes
+  end
+
+  def list(position : Int32)
+    condition = ->(array : Array(Int32)) { !(array.size == position) }
+
+    self.calculate(condition)
+  end
+
+  def at(position : Int32)
+    condition = ->(array : Array(Int32)) { !(array.size == position) }
+
+    self.calculate(condition)[-1]
+  end
+
+  private def self.calculate(condition : Proc(Array(Int32), Bool))
+    cur_num = 2
+    primes = [2]
+
+    while condition.call(primes)
       cur_num += 1
       primes.push(cur_num) if primes.all? { |p| !cur_num.divisible_by?(p) }
     end
@@ -24,10 +40,18 @@ module PrimeNumber
   end
 end
 
-it "should return the first 25 prime numbers" do
-  PrimeNumber.list(25).should eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
-end
+describe PrimeNumber do
+  primes_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-it "should return the prime number at position 1001st" do
-  PrimeNumber.at(10001).should eq(104743)
+  it "should return the first 25 prime numbers" do
+    PrimeNumber.list(25).should eq(primes_100)
+  end
+
+  it "should return the prime number at position 1001st" do
+    PrimeNumber.at(10001).should eq(104743)
+  end
+
+  it "should return all primes below 100" do
+    PrimeNumber.below(100).should eq(primes_100)
+  end
 end
